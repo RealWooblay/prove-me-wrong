0492df88c95ddd59a8fcad9f09ed036ffa3de15a
ParanoidSyntaxError <git@jbojtor.com>
2025-07-04 23:04:52 +0200

    Remove all files related to the Blink scaffold project, including configuration, source code, assets, and documentation.

---
+++ contracts/contracts/IPMW20.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
+import {IERC1363} from "@openzeppelin/contracts/interfaces/IERC1363.sol";
+
+interface IPMW20 is IERC20, IERC20Metadata, IERC20Permit, IERC1363 {
+    function initialize(
+        string memory initName,
+        string memory initSymbol,
+        address initOwner
+    ) external;
+
+    function mint(address to, uint256 amount) external;
+
+    function burn(uint256 amount) external;
+
+    function burnFrom(address from, uint256 amount) external;
+}
---
+++ contracts/contracts/PMW20.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {ERC20, IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Permit, IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC1363} from "@openzeppelin/contracts/token/ERC20/extensions/ERC1363.sol";
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+
+import {IPMW20} from "./IPMW20.sol";
+
+contract PMW20 is
+    ERC20,
+    ERC20Burnable,
+    ERC20Permit,
+    ERC1363,
+    Ownable,
+    Initializable,
+    IPMW20
+{
+    string private _name;
+    string private _symbol;
+
+    constructor() ERC20("", "") ERC20Permit("") Ownable(address(1)) {}
+
+    function initialize(
+        string memory initName,
+        string memory initSymbol,
+        address initOwner
+    ) external override initializer {
+        _name = initName;
+        _symbol = initSymbol;
+
+        _transferOwnership(initOwner);
+    }
+
+    function mint(address to, uint256 amount) external override onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public override(ERC20Burnable, IPMW20) {
+        ERC20Burnable.burn(amount);
+    }
+
+    function burnFrom(
+        address from,
+        uint256 amount
+    ) public override(ERC20Burnable, IPMW20) {
+        super.burnFrom(from, amount);
+    }
+
+    function name()
+        public
+        view
+        override(ERC20, IERC20Metadata)
+        returns (string memory)
+    {
+        return _name;
+    }
+
+    function symbol()
+        public
+        view
+        override(ERC20, IERC20Metadata)
+        returns (string memory)
+    {
+        return _symbol;
+    }
+
+    function nonces(
+        address owner
+    ) public view override(ERC20Permit, IERC20Permit) returns (uint256) {
+        return super.nonces(owner);
+    }
+}
